26752d102b5a3883c4392e5c86ab39ed0b312faa
luoxianjun <luoxianjun@cumt.edu.cn>
2024-06-06 15:02:23 +0800

    缓存定时删除处理

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -3,7 +3,6 @@ package com.sky.mapper;
 import com.sky.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 
-import java.time.LocalDateTime;
 import java.util.Map;
 
 @Mapper
@@ -11,8 +10,7 @@ public interface UserMapper {
     User getByOpenid(String openid);
 
     void insert(User user);
-
-
+    
     User getById(Long userId);
 
     Integer countByMap(Map<String, Object> map);
--- sky-server/src/main/java/com/sky/task/OrderTask.java
+++ sky-server/src/main/java/com/sky/task/OrderTask.java
@@ -3,12 +3,14 @@ package com.sky.task;
 import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Component
@@ -17,6 +19,9 @@ public class OrderTask {
 
     @Resource
     private OrderMapper orderMapper;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
 
     @Scheduled(cron = "0 * * * * ? ")
@@ -46,5 +51,15 @@ public class OrderTask {
 
         List<Long> idList = ordersList.stream().map(Orders::getId).collect(Collectors.toList());
         orderMapper.cancelOrderById(idList, Orders.COMPLETED, null, null);
+    }
+
+    @Scheduled(cron = "0 0/30 * * * ? ")
+    public void deleteStatisticsCache() {
+        log.info("deleteStatisticsCache");
+        Set<String> keys = stringRedisTemplate.keys("statistics*");
+        if (keys == null || keys.isEmpty()) {
+            return;
+        }
+        stringRedisTemplate.delete(keys);
     }
 }
